e0be17af97328b04ee45cc288001d2d8d1844d00
weijun8687 <812085814@qq.com>
2018-04-06 15:11:04 +0800

    Initial Commit

---
+++ Encryption/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Encryption
+//
+//  Created by WJ on 2018/4/6.
+//  Copyright © 2018年 WJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
