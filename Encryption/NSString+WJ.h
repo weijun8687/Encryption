107686b176bee8ed7f64eb780861fde101d8c3c5
weijun8687 <812085814@qq.com>
2018-04-07 14:39:38 +0800

    整理

--- Encryption/NSString+WJ.h
+++ Encryption/NSString+WJ.h
@@ -10,9 +10,28 @@
 
 @interface NSString (WJ)
 
+//  rsa 加密
+- (NSString *)enodeByRsa;
+// rsa 解密
+- (NSString *)dencodeByRsa;
+
+// 3des 加密
+- (NSString *)encodeBy3des;
+// 3des 解密
+- (NSString *)dencodeBy3des;
+
+// md5 加密
+- (NSString *)encodeBymd5;
+// md5 解密
+- (NSString *)dencodeBymd5;
+
+// base64 编码
+- (NSString *)encodeByBase64;
+// base64 解码
+- (NSString *)dencodeByBase64;
+
 // 通过 AES 加密
 - (NSString *)encodeByAES;
-
 // 通过 AES 解密
 - (NSString *)dencodeByAES;
 
--- Encryption/NSString+WJ.m
+++ Encryption/NSString+WJ.m
@@ -9,6 +9,15 @@
 #import "NSString+WJ.h"
 #import <CommonCrypto/CommonCryptor.h>
 #import "GTMBase64.h"
+#import <CommonCrypto/CommonDigest.h>   // MD5 加密时使用
+#import "JKEncrypt.h"      // 3des 加密时使用
+#import "RSA.h"     // rsa 加密时使用
+
+#define PUBLIC_KEY @"MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDf2jkCy0wrV0LHHsU7R5lZNV0njue7+7ul2HHS4KQeLZsmS/vEphUB9afpfmU2glvqfes9mABx+mioCYvCK7DIxiFAZL54dGy+Ujwiibrv7dzBGXo3u1aS3KxQ/aK7I1ny4Ul5CujruokTbATeleBsJFPGLXRbGP29DLfN+Xss0wIDAQAB"
+#define PRIVATE_KEY @"MIICdgIBADANBgkqhkiG9w0BAQEFAASCAmAwggJcAgEAAoGBAN/aOQLLTCtXQscexTtHmVk1XSeO57v7u6XYcdLgpB4tmyZL+8SmFQH1p+l+ZTaCW+p96z2YAHH6aKgJi8IrsMjGIUBkvnh0bL5SPCKJuu/t3MEZeje7VpLcrFD9orsjWfLhSXkK6Ou6iRNsBN6V4GwkU8YtdFsY/b0Mt835eyzTAgMBAAECgYAbmFw00vj10tEBmPJ5Z08pJyHvsXcxvkuYm0EU3Op+aeLZar6DtHGchzsG1rUFbjaEhrdMJYPQiS0DogGfkyE8s9hEiAFkXWr0LK+1nQNnFc6S3+Anqm1KQ5RhKN79dQc1D7mCIeG+bfU/eNpY+IdBatqyn5IV/FNOVHr4pumm8QJBAPhiXyr8ZOo3bi6DVowskhPp0LLjIGNcrgCHjegPIn1FNifA4wAn+NW5YfvO2ttuPRWejLt6blYQxBh5uLKuzSkCQQDmt0tunVbZancYKOVxqDhb4HnAAp65Yje5zlwPxees/9pCXz0t1dIbaxrLcZ+h+fb3w0dAldbiYbf4eqIIxu2bAkEAot7uhKNoEOU0DK/2qof3abNiNEsWy9DUEGjStp5mATrHHh4vO8T6ODsNcy7a+BQ7XdfPdIf9ndX0oBAA+roAsQJAD8ytlb2gnPL1hOoIDGiAs4oDzGphhEB9oHPJSis7WlWLFNCA2Aq0gLws8ZGuZOFBUGZHEt0wAgC/IH0Fa6Rz0QJAL/oc5wi36EhO6gFd8QLbFZmVN19UoiC10jiDIiW6zi6SribGMs7JrC00dJPdaxp3ggtZU24GlQqEmUXqTDvUiA=="
+
+// 没有转换的私钥(不能用)
+//#define PRIVATE_KEY @"MIICXAIBAAKBgQDf2jkCy0wrV0LHHsU7R5lZNV0njue7+7ul2HHS4KQeLZsmS/vEphUB9afpfmU2glvqfes9mABx+mioCYvCK7DIxiFAZL54dGy+Ujwiibrv7dzBGXo3u1aS3KxQ/aK7I1ny4Ul5CujruokTbATeleBsJFPGLXRbGP29DLfN+Xss0wIDAQABAoGAG5hcNNL49dLRAZjyeWdPKSch77F3Mb5LmJtBFNzqfmni2Wq+g7RxnIc7Bta1BW42hIa3TCWD0IktA6IBn5MhPLPYRIgBZF1q9CyvtZ0DZxXOkt/gJ6ptSkOUYSje/XUHNQ+5giHhvm31P3jaWPiHQWrasp+SFfxTTlR6+KbppvECQQD4Yl8q/GTqN24ug1aMLJIT6dCy4yBjXK4Ah43oDyJ9RTYnwOMAJ/jVuWH7ztrbbj0Vnoy7em5WEMQYebiyrs0pAkEA5rdLbp1W2Wp3GCjlcag4W+B5wAKeuWI3uc5cD8XnrP/aQl89LdXSG2say3Gfofn298NHQJXW4mG3+HqiCMbtmwJBAKLe7oSjaBDlNAyv9qqH92mzYjRLFsvQ1BBo0raeZgE6xx4eLzvE+jg7DXMu2vgUO13Xz3SH/Z3V9KAQAPq6ALECQA/MrZW9oJzy9YTqCAxogLOKA8xqYYRAfaBzyUorO1pVixTQgNgKtIC8LPGRrmThQVBmRxLdMAIAvyB9BWukc9ECQC/6HOcIt+hITuoBXfEC2xWZlTdfVKIgtdI4gyIlus4ukq4mxjLOyawtNHST3Wsad4ILWVNuBpUKhJlF6kw71Ig="
 
 // aes 加密的 key 换成自己的
 static NSString *const AES_KEY = @"AES_KEY";
@@ -16,6 +25,77 @@ static NSString *const AES_KEY = @"AES_KEY";
 static NSString *const AES_IV = @"AES_IV";
 
 @implementation NSString (WJ)
+
+#pragma mark - rsa 加密
+// 生成私钥和公钥
+// 新建ssh 文件夹 , 在终端中 进入到该文件夹下 cd + 文件路径
+// 输入命令: openssl
+// 输入生成私钥命令:  genrsa -out rsa_private_key.pem 1024
+// 将私钥转成PKCS8的格式(必须执行): pkcs8 -topk8 -inform PEM -in rsa_private_key.pem -outform PEM -nocrypt
+// 将终端输出的私钥复制, 保存, 用于加/解密
+// 生成公钥:  rsa -in rsa_private_key.pem -pubout -out rsa_public_key.pem
+// 直接用文本打开公钥, 用于加/解密
+// 既可以使用公钥加密, 也可以使用私钥加密, 但是 一定要配套使用
+
+- (NSString *)enodeByRsa{
+    // 使用公钥加密
+    //    return [RSA encryptString:self publicKey:PUBLIC_KEY];
+    // 使用私钥加密
+    return [RSA encryptString:self privateKey:PRIVATE_KEY];
+}
+
+- (NSString *)dencodeByRsa{
+    
+    // 使用私钥解密
+    //    return [RSA decryptString:self privateKey:PRIVATE_KEY];
+    // 使用公钥解密
+    return [RSA decryptString:self publicKey:PUBLIC_KEY];
+}
+
+#pragma mark - 3des 加密
+// 使用时 将 JKEncrypt.m 中的秘钥 和 偏移量 换成 自己的
+- (NSString *)encodeBy3des{
+    JKEncrypt *jk = [[JKEncrypt alloc] init];
+    return [jk doEncryptStr:self];;
+}
+
+#pragma mark - 3des 解密
+- (NSString *)dencodeBy3des{
+    JKEncrypt *jk = [[JKEncrypt alloc] init];
+    return [jk doDecEncryptStr:self];;
+}
+
+
+#pragma mark - md5 加密
+// 使用 md5 加密时, 经常使用加盐  技术
+- (NSString *)encodeBymd5{
+    const char *cStr = [self UTF8String];
+    unsigned char digest[CC_MD5_DIGEST_LENGTH];
+    CC_MD5(cStr, (CC_LONG)strlen(cStr), digest);
+    NSMutableString *result = [NSMutableString stringWithCapacity:CC_MD5_DIGEST_LENGTH * 2];
+    for (int i = 0; i < CC_MD5_DIGEST_LENGTH; i++) {
+        [result appendFormat:@"%02X", digest[i]];
+    }
+    return result;
+}
+
+#pragma mark - base64 编码
+- (NSString *)encodeByBase64{
+    
+    NSData *data = [self dataUsingEncoding:NSUTF8StringEncoding];
+    NSData *base64Data = [data base64EncodedDataWithOptions:0];
+    NSString *baseString = [[NSString alloc]initWithData:base64Data encoding:NSUTF8StringEncoding];
+    return baseString;
+}
+
+#pragma mark - base64 解码
+- (NSString *)dencodeByBase64{
+    
+    NSData *data = [[NSData alloc]initWithBase64EncodedString:self options:NSDataBase64DecodingIgnoreUnknownCharacters];
+    NSString *string = [[NSString alloc]initWithData:data encoding:NSUTF8StringEncoding];
+    return string;
+}
+
 
 - (NSString *)encodeByAES{
     NSData *data = [self dataUsingEncoding:NSUTF8StringEncoding];
